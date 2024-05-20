79866cf4867a7c2f14dc96291bbd1740b2832cda
Amirovsherbeknew <amirovsherbek55@gmail.com>
2024-05-20 05:31:51 +0500

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,72 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useForm, FormProvider } from 'react-hook-form';
 
 function App() {
+  const methods = useForm();
+
+  const onSubmit = async (data:any) => {
+    try {
+      // Formdan malumotlarni yuborish
+      console.log(data);
+    } catch (error) {
+      // Xatolikni yuqori darajada yoritish
+      alert('Xatolik: ');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>
+            <input
+              type="email"
+              id="email"
+              className={`input ${methods.formState.errors.email ? 'red-border' : ''}`}
+              {...methods.register('email', {
+                required: {
+                  value: true,
+                  message: 'Email manzilni kiriting',
+                },
+              })}
+            />
+            <select  value={undefined} className={`input ${methods.formState.errors.gender ? 'red-border' : ''}`}
+              {...methods.register('gender', {
+                required: {
+                  value: true,
+                  message: 'gender  kiriting',
+                },
+              })}>
+                <option value="">Tanlang...</option>
+              <option value="value1">label1</option>
+              <option value="value2">label2</option>
+              <option value="valu3">label3</option>
+            </select>
+            <input
+              type="number"
+              id="age"
+              className={`input ${methods.formState.errors.age ? 'red-border' : ''}`}
+              {...methods.register('age', {
+                required: {
+                  value: true,
+                  message: 'Yoshni kiriting',
+                },
+              })}
+            />
+            <input
+              type="text"
+              id="name"
+              className={`input ${methods.formState.errors.name ? 'red-border' : ''}`}
+              {...methods.register('name', {
+                required: {
+                  value: true,
+                  message: 'Ismni kiriting',
+                },
+              })}
+            />
+            <button type="submit">Submit</button>
+          </form>
+    </FormProvider>
   );
 }
 
